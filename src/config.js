a84cb519dbdf7517a5ac769741073126c208a28a
Raviraj Danidhariya <ravirajdanidhariya@Techs-MacBook-Pro.local>
2019-12-10 12:33:54 +0530

    setup react-app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-2",
+      BUCKET: "notes-app-api-prod-serverlessdeploymentbucket-pujsa442pqvd"
+    },
+    apiGateway: {
+      REGION: "us-east-2",
+      URL: "https://4c62gb6th4.execute-api.us-east-2.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-east-2",
+      USER_POOL_ID: "us-east-2_Nt2Wlt5dN",
+      APP_CLIENT_ID: "4omgoje51rp54ubh05lv5gus3k",
+      IDENTITY_POOL_ID: "us-east-2:12100a51-d95e-42f9-b122-e00bdfae2d4f"
+    }
+  };
