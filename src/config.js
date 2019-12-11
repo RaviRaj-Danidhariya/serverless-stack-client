e2db79737fec7e8732859c264a090e3fafadb04a
Raviraj Danidhariya <ravirajdanidhariya@Techs-MacBook-Pro.local>
2019-12-11 11:00:58 +0530

    updated project

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 export default {
+    MAX_ATTACHMENT_SIZE: 5000000,
     s3: {
       REGION: "us-east-2",
       BUCKET: "notes-app-api-prod-serverlessdeploymentbucket-pujsa442pqvd"
@@ -9,8 +10,8 @@ export default {
     },
     cognito: {
       REGION: "us-east-2",
-      USER_POOL_ID: "us-east-2_Nt2Wlt5dN",
-      APP_CLIENT_ID: "4omgoje51rp54ubh05lv5gus3k",
+      USER_POOL_ID: "us-east-2_U1WCj9aFW",
+      APP_CLIENT_ID: "5cm5p89s7f2fh77dlbprstn442",
       IDENTITY_POOL_ID: "us-east-2:12100a51-d95e-42f9-b122-e00bdfae2d4f"
     }
   };
--- src/routes.js
+++ src/routes.js
@@ -2,13 +2,19 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home";
 import NotFound from "./containers/NotFound";
-
-export default function Routes() {
-  return (
-    <Switch>
-      <Route path="/" exact component={Home} />
-      { /* Finally, catch all unmatched routes */ }
-      <Route component={NotFound} />
-    </Switch>
-  );
-}
+import Login from "./containers/Login";
+import Signup from "./containers/Signup";
+import AppliedRoute from "./components/AppliedRoutes";
+import NewNote from "./containers/NewNote";
+export default function Routes({ appProps }) {
+    return (
+      <Switch>
+        <AppliedRoute path="/" exact component={Home} appProps={appProps} />
+        <AppliedRoute path="/login" exact component={Login} appProps={appProps} />
+        <AppliedRoute path="/signup" exact component={Signup} appProps={appProps} />
+        <AppliedRoute path="/notes/new" exact component={NewNote} appProps={appProps} />
+        { /* Finally, catch all unmatched routes */ }
+        <Route component={NotFound} />
+      </Switch>
+    );
+  }
